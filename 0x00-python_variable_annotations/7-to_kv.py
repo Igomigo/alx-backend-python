438a7ec6aa1b95e0cbb815d90c210b34ad6b793b
Igomigo <igomigofatai@gmail.com>
2023-12-13 04:14:54 +0000

    Taask 7 updated

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,4 +6,4 @@ from typing import Union, Tuple
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
     """ returns a tuple with str as the first element and float
     or int as second element of the tuple"""
-    return (k, v)
+    return (k, v**2)
