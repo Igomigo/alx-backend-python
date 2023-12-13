5624e6b866793253ff25ef35c67905ecd68b0622
Igomigo <igomigofatai@gmail.com>
2023-12-13 01:44:22 +0000

    string concate function done

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Script that runs the concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that takes two strings as arguments and returns
+    a concatenated string"""
+    return str1 + str2
