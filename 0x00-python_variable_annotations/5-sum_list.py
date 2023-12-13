34da45e3befee973ba00870883e9b19e0099e28c
Igomigo <igomigofatai@gmail.com>
2023-12-13 02:33:44 +0000

    Sum list task done

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+""" Script that runs the sum_list function"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """ Returns the sum of the list items as a float """
+    return sum(input_list)
